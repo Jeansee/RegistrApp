5aee54f9a18913d9390613cd65363b6e1cbe001c
Jeansee <jea.sanchez@duocuc.cl>
2023-09-02 22:57:06 -0400

    tercer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'registro',
     loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
   },
+  {
+    path: 'recuperar',
+    loadChildren: () => import('./recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+  },
 ];
 
 @NgModule({
