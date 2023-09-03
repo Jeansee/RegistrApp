ff7cc4198ae7716e472e7efc82079f441fa3f2c0
Jeansee <jea.sanchez@duocuc.cl>
2023-09-02 21:16:35 -0400

    segundo commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  irRegistro(){
+    this.router.navigate(["/registro"])
+  }
+
+    nombre = "";
+    contra = "";
 
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -11,5 +12,6 @@ export class RegistroPage implements OnInit {
 
   ngOnInit() {
   }
+
 
 }
