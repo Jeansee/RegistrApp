45b1f30e92bddfb23e5cf42011d09d98b6f4b2b2
jazmgou <yasant7@gmail.com>
2023-10-19 20:56:01 -0300

    localstorage arreglar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.grabar_localstorage();
+  }
+
+  grabar_localstorage() {
+    // Genera un nombre único o secuencial (puedes personalizar esta lógica según tus necesidades)
+    const nombre = this.generarNombreUnico();
+
+    let persona = {
+      nombre: nombre,
+    }
+
+    localStorage.setItem("nombre", persona.nombre);
+  }
+
+  // Función para generar un nombre único (puedes personalizar esta lógica)
+  generarNombreUnico() {
+    // Genera un número aleatorio único usando un timestamp
+    const timestamp = Date.now();
+    const nombreUnico = `Usuario_${timestamp}`;
+
+    return nombreUnico;
+  }
 }
