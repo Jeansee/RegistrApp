9564e0b17edb0290fcdb7347a3afb2d9d5f6a225
Victoria <vi.saavedrap@duocuc.cl>
2023-10-19 22:47:37 -0300

    cambios para no ingresar 🚫

--- src/app/coso-noingresado.guard.ts
+++ src/app/coso-noingresado.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CosoNoingresadoGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
-  }
+//restringe la entrada a página si no esta iniciado con un usuario
+export class NoIngresadoGuard implements CanActivate {
 
+  constructor(public navCtrl: NavController){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(localStorage.getItem('ingresado')){
+        this.navCtrl.navigateRoot('inicio');
+        return false;
+      }else{
+        return true;
+      }
+  }
+  
 }
